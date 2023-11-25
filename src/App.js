00560d744f5247f74332214ebfb26ac54fc142d4
james.sullivan <james.sullivan@axomic.com>
2023-11-25 12:48:58 -0500

    got rid of cart page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ProductShow from './Pages/ProductShow/ProductShow';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './Pages/Home/Home';
-import Cart from './Pages/Cart/Cart'
 import Checkout from './Pages/Checkout/Checkout';
 import { useEffect, useState } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
@@ -20,7 +19,6 @@ function App() {
     <Routes>
       <Route exact path="/Product/:id" element={<ProductShow/>}/>
       <Route exact path="/" element={<Home/>}/>  
-      <Route exact path="/Cart" element={<Cart/>}/>
       <Route exact path="/Checkout" element={<Checkout/>}/>
       </Routes>
       <Footer/>
